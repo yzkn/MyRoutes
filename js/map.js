7be132e9ae421351b739990cd17ddd714b8cd0ac
YA <9039417+yzkn@users.noreply.github.com>
2024-06-14 12:37:43 +0900

    Update map.js

--- js/map.js
+++ js/map.js
@@ -15,15 +15,25 @@ var baseMaps = {
         {
             attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
         }),
+
     'OSM': L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
         {
             attribution: '© OpenStreetMap contributors'
         }),
+
     '地理院タイル': L.tileLayer('https://cyberjapandata.gsi.go.jp/xyz/std/{z}/{x}/{y}.png',
         {
             attribution: '出典：国土地理院ウェブサイト(https://maps.gsi.go.jp/development/ichiran.html)　この地理院タイルは基本測量成果（名称：電子地形図（タイル））です。'
         }),
     '淡色地図': L.tileLayer('https://cyberjapandata.gsi.go.jp/xyz/pale/{z}/{x}/{y}.png',
+        {
+            attribution: '出典：国土地理院ウェブサイト(https://maps.gsi.go.jp/development/ichiran.html)　この地理院タイルは基本測量成果（名称：電子地形図（タイル））です。'
+        }),
+    '航空写真': L.tileLayer('https://cyberjapandata.gsi.go.jp/xyz/seamlessphoto/{z}/{x}/{y}.png',
+        {
+            attribution: '出典：国土地理院ウェブサイト(https://maps.gsi.go.jp/development/ichiran.html)　この地理院タイルは基本測量成果（名称：電子地形図（タイル））です。'
+        }),
+    '白地図': L.tileLayer('https://cyberjapandata.gsi.go.jp/xyz/blank/{z}/{x}/{y}.png',
         {
             attribution: '出典：国土地理院ウェブサイト(https://maps.gsi.go.jp/development/ichiran.html)　この地理院タイルは基本測量成果（名称：電子地形図（タイル））です。'
         }),
@@ -35,7 +45,16 @@ var baseMaps = {
         {
             attribution: '出典：国土地理院ウェブサイト(https://maps.gsi.go.jp/development/ichiran.html)　この地理院タイルは基本測量成果です。'
         }),
+    '標準地図（英語）': L.tileLayer('https://cyberjapandata.gsi.go.jp/xyz/english/{z}/{x}/{y}.png',
+        {
+            attribution: '出典：国土地理院ウェブサイト(https://maps.gsi.go.jp/development/ichiran.html)　この地理院タイルは基本測量成果（名称：電子地形図（タイル））です。'
+        }),
+
     'Google Maps': L.tileLayer('https://mt1.google.com/vt/lyrs=m&x={x}&y={y}&z={z}',
+        {
+            attribution: '&copy; Google'
+        }),
+    'Google Roads': L.tileLayer('https://mt1.google.com/vt/lyrs=h&x={x}&y={y}&z={z}',
         {
             attribution: '&copy; Google'
         }),
@@ -46,6 +65,47 @@ var baseMaps = {
     'Google Satellite Hybrid': L.tileLayer('https://mt1.google.com/vt/lyrs=y&x={x}&y={y}&z={z}',
         {
             attribution: '&copy; Google'
+        }),
+    'Google Streets': L.tileLayer('https://mt1.google.com/vt/lyrs=r&x={x}&y={y}&z={z}',
+        {
+            attribution: '&copy; Google'
+        }),
+    'Google Terrain': L.tileLayer('https://mt1.google.com/vt/lyrs=t&x={x}&y={y}&z={z}',
+        {
+            attribution: '&copy; Google'
+        }),
+};
+
+var overlayMaps = {
+    'OSM 20%': L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+        {
+            attribution: '© OpenStreetMap contributors',
+            opacity: 0.2
+        }),
+    'OSM 40%': L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+        {
+            attribution: '© OpenStreetMap contributors',
+            opacity: 0.4
+        }),
+    '地理院タイル 20%': L.tileLayer('https://cyberjapandata.gsi.go.jp/xyz/std/{z}/{x}/{y}.png',
+        {
+            attribution: '出典：国土地理院ウェブサイト(https://maps.gsi.go.jp/development/ichiran.html)　この地理院タイルは基本測量成果（名称：電子地形図（タイル））です。',
+            opacity: 0.2
+        }),
+    '地理院タイル 40%': L.tileLayer('https://cyberjapandata.gsi.go.jp/xyz/std/{z}/{x}/{y}.png',
+        {
+            attribution: '出典：国土地理院ウェブサイト(https://maps.gsi.go.jp/development/ichiran.html)　この地理院タイルは基本測量成果（名称：電子地形図（タイル））です。',
+            opacity: 0.4
+        }),
+    'Google Maps 20%': L.tileLayer('https://mt1.google.com/vt/lyrs=m&x={x}&y={y}&z={z}',
+        {
+            attribution: '&copy; Google',
+            opacity: 0.2
+        }),
+    'Google Maps 40%': L.tileLayer('https://mt1.google.com/vt/lyrs=m&x={x}&y={y}&z={z}',
+        {
+            attribution: '&copy; Google',
+            opacity: 0.4
         })
 };
 
@@ -103,7 +163,8 @@ function initMap() {
         center: [35.681236, 139.767125],
         zoom: 11
     }).addLayer(baseMaps['Esri(航空写真)']);
-    L.control.layers(baseMaps).addTo(map);
+    // L.control.layers(baseMaps).addTo(map);
+    L.control.layers(baseMaps, overlayMaps).addTo(map);
 
     var style = {
         color: 'red',
