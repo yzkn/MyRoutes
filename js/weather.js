25ad0e42ff4458008ae5adf34e86508bd4d14ac1
YA <9039417+yzkn@users.noreply.github.com>
2024-06-28 18:45:16 +0900

    Updates

--- js/weather.js
+++ js/weather.js
@@ -4,11 +4,9 @@ const apiTargetLocations = ['秩父市', '飯能市', '所沢市'];
 
 
 const retrieve = async _ => {
-    console.log('retrieve');
-
     fetch(
         // apiLocationUrl + apiTargetLocations.join(',')
-        "http://localhost/GitHub/MyRoutes/%E7%A7%A9%E7%88%B6%E5%B8%82,%E9%A3%AF%E8%83%BD%E5%B8%82,%E6%89%80%E6%B2%A2%E5%B8%82.json?1"
+        "http://localhost/GitHub/MyRoutes/%E7%A7%A9%E7%88%B6%E5%B8%82,%E9%A3%AF%E8%83%BD%E5%B8%82,%E6%89%80%E6%B2%A2%E5%B8%82.json"
         , {
             headers: {
                 Accept: "application/json",
@@ -17,32 +15,23 @@ const retrieve = async _ => {
         })
         .then((response) => response.json())
         .then((jsonData) => {
-            // console.log({ jsonData });
-
             Object.keys(jsonData).forEach((city) => {
-
-                // console.log('jsonData[city]', jsonData[city]);
-
                 let result = `<div><div title="${jsonData[city].hall.lat},${jsonData[city].hall.long}">${city}</div>`;
 
                 let tjResult = '<div class="tj">';
                 Object.keys(jsonData[city].tj).forEach((tjDate) => {
-                    // console.log('jsonData[city].tj[tjDate]', jsonData[city].tj[tjDate]);
-                    tjResult += `<img class="weatherIcon" title="${tjDate}" src="${jsonData[city].tj[tjDate].weatherIcon}">`;
+                    tjResult += `<img class="weatherIcon" title="${tjDate} ${jsonData[city].tj[tjDate].temperature}℃" src="${jsonData[city].tj[tjDate].weatherIcon}">`;
                 });
                 tjResult += `</div>`;
 
                 let wnResult = '<div class="wn">';
                 Object.keys(jsonData[city].wn).forEach((wnDate) => {
-                    wnResult += `<img class="weatherIcon" title="${wnDate}" src="${jsonData[city].wn[wnDate].weatherIcon}">`;
+                    wnResult += `<img class="weatherIcon" title="${wnDate} ${jsonData[city].wn[wnDate].temperature}℃" src="${jsonData[city].wn[wnDate].weatherIcon}">`;
                 });
                 wnResult += `</div>`;
 
                 result += tjResult + wnResult;
                 result += `</div>`;
-
-
-                console.log(result);
 
 
                 // マーカー
@@ -58,7 +47,7 @@ const retrieve = async _ => {
                         jsonData[city].hall.long
                     ],
                     { icon: divIcon }).addTo(map);
-                marker.bindPopup(result, { autoClose: false, minWidth: 600 }).openPopup();
+                marker.bindPopup(result, { autoClose: false, minWidth: 400 }).openPopup();
                 // マーカー
             });
 
@@ -68,15 +57,12 @@ const retrieve = async _ => {
 
 
 const checkVersion = _ => {
-    console.log('checkVersion');
-
     fetch(apiBaseUrl, {
         headers: {
             Accept: "application/json",
             "Content-Type": "application/json;charset=utf-8"
         }
     }).then(response => {
-        // console.log(response);
         if (response.status == 200) {
             retrieve();
         }
@@ -85,8 +71,6 @@ const checkVersion = _ => {
 
 
 const main = _ => {
-    console.log('main');
-
     checkVersion();
 };
 
