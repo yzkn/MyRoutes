16dfcab5d1139dad5f353b6f02dc48c6a8b7c937
YA <9039417+yzkn@users.noreply.github.com>
2024-10-05 13:27:42 +0900

    init

--- js/map.js
+++ js/map.js
@@ -440,116 +440,7 @@ var overlayMaps = {
 };
 
 
-// Util
-function mapmove() {
-    if (map != null) {
-        const latitude = document.getElementById('latitude').value;
-        const longitude = document.getElementById('longitude').value;
-        const zoom = document.getElementById('zoom').value;
-        map.panTo(new L.LatLng(roundlatlng(parseFloat(latitude)), roundlatlng(parseFloat(longitude))));
-        map.setZoom(parseInt(zoom));
-        console.log(
-            'map.panTo()',
-            roundlatlng(parseFloat(latitude)), roundlatlng(parseFloat(longitude)), parseInt(zoom)
-        );
-    }
-}
-
-function roundlatlng(val) {
-    const base = 0.0000001 // 約1cm相当の経緯度
-    return Math.round(val / base) * base;
-}
-
-
 // Initialize
-function initMenu() {
-    var latlngzoom = document.getElementById('latlngzoom');
-    latlngzoom.innerHTML = `
-        <form>
-            <div class="form-group">
-                <label for="latitude">Latitude</label>
-                <input type="number" step="0.000001" min="-90" max="90" class="form-control" id="latitude" placeholder="Enter latitude" value="35.681236">
-            </div>
-            <div class="form-group">
-                <label for="longitude">Longitude</label>
-                <input type="number" step="0.000001" min="-180" max="180" class="form-control" id="longitude" placeholder="longitude" value="139.767125">
-            </div>
-            <div class="form-group">
-                <label for="zoom">Zoom level</label>
-                <input type="range" step="1" min="1" max="30" class="form-control-range" id="zoom" value="11">
-            </div>
-            <button type="button" class="btn btn-primary" id="mapmove">Move</button>
-        </form>`;
-
-    const mapmoveElement = document.getElementById('mapmove');
-    mapmoveElement.addEventListener('click', (event) => {
-        mapmove();
-    });
-}
-
-function initMap() {
-    L = window.L;
-    map = L.map('map', {
-        center: [35.681236, 139.767125],
-        zoom: 11
-    }).addLayer(baseMaps['Esri(航空写真)']);
-    // L.control.layers(baseMaps).addTo(map);
-    L.control.layers(baseMaps, overlayMaps).addTo(map);
-
-    var style = {
-        color: 'red',
-        opacity: 1.0,
-        fillOpacity: 1.0,
-        weight: 1,
-        clickable: false
-    };
-    L.Control.FileLayerLoad.LABEL = '<img class="icon" src="img/pin.svg" />';
-    control = L.Control.fileLayerLoad({
-        fileSizeLimit: 1024000,
-        fitBounds: true,
-        layerOptions: {
-            style: style,
-            pointToLayer: function (data, latlng) { }
-        }
-    });
-    control.addTo(map);
-    control.loader.on('data:loaded', function (e) {
-        var layer = e.layer;
-        console.log(layer);
-    });
-    control.loader.on('data:error', function (error) {
-        console.error(error);
-    });
-
-    L.control.BigImage({
-        downloadTitle: 'Download',
-        inputTitle: ' Download your png file: ',
-        maxScale: 3,
-        position: 'topleft',
-        printControlTitle: 'Capture image',
-        title: 'Capture image',
-    }).addTo(map);
-
-    L.easyButton('<img class="icon" src="img/share.svg" title="Share this map (You need to use Chrome for Android.)"/>', function (btn, map) {
-        shareImage();
-    }).addTo(map);
-
-    map.on('moveend', function (e) {
-        const latitude = document.getElementById('latitude');
-        const longitude = document.getElementById('longitude');
-        const zoom = document.getElementById('zoom');
-        if (latitude && longitude && zoom) {
-            console.log(
-                'moveend',
-                map.getCenter().lat, map.getCenter().lng, map.getZoom(),
-                latitude.value, longitude.value, zoom.value
-            );
-            latitude.value = roundlatlng(map.getCenter().lat);
-            longitude.value = roundlatlng(map.getCenter().lng);
-            zoom.value = map.getZoom();
-        }
-    });
-}
 
 function initMap2() {
     L = window.L;
@@ -624,18 +515,6 @@ function initMap2() {
         shareImage();
     }).addTo(map);
 }
-
-function initMiniMap() {
-    new L.Control.MiniMap(baseMaps['地理院タイル'], { toggleDisplay: true }).addTo(map);
-}
-
-function initGeoman() {
-    map.pm.addControls({
-        position: 'topleft'
-    });
-}
-
-//
 
 function shareImage() {
     domtoimage.toPng(document.getElementsByClassName('leaflet-pane leaflet-map-pane')[0])
