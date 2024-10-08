2f2b2d9dd34d56a91e966737aaff2a1a42bc1f98
YA <9039417+yzkn@users.noreply.github.com>
2024-10-08 12:22:47 +0900

    Update revgeocoding.js

--- js/geo/revgeocoding.js
+++ js/geo/revgeocoding.js
@@ -1,33 +1,11 @@
 const reverseGeocoding = async (lat, lon) => {
-    const result = await getMuniCd(lat, lon);
-    const muniCd = result?.muniCd;
-    const lv01Nm = result?.lv01Nm;
-    if (!muniCd && !lv01Nm) {
-        return '---';
-    }
-
-    return getPrefecture(muniCd) + lv01Nm;
-};
-
-const getMuniCd = async (lat, lon) => {
     const response = await fetch(
-        `https://mreversegeocoder.gsi.go.jp/reverse-geocoder/LonLatToAddress?lat=${lat}&lon=${lon}`
+        `https://revgeocoding.yuz.workers.dev/?lat=${lat}&lon=${lon}`
     );
     if (!response.ok) {
         return undefined;
     }
 
-    const lonLatToAddress = await response.json();
-    return lonLatToAddress.results;
-};
-
-const getPrefecture = (muniCdInput) => {
-    const muniCd =
-        muniCdInput.substring(0, 1) === "0" ? muniCdInput.slice(1) : muniCdInput;
-    const muniContents = MUNI_ARRAY[muniCd];
-    if (!muniContents) {
-        return undefined;
-    }
-
-    return muniContents.split(",")[3];
+    const result = await response.json();
+    return result.city + result.lv01Nm;
 };
